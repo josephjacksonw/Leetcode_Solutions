02706035c0a2f5616de5106bbad14e75c085d2a2
josephjacksonw <josephjacksonw@gmail.com>
2023-08-30 10:10:41 -0400

    2824 count pairs whose sum is less than target

---
+++ 2824 count pairs whose sum is less than target/2824 count pairs whose sum is less than target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+  count = 0
+  for (i=0; i<nums.length-1; i++) {
+      for (j=i+1; j<nums.length; j++) {
+          if (nums[i] + nums[j] < target) {
+              count ++
+          }
+      }
+  }
+  return count
+};
